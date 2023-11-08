ba0e5364b733b883268d08e7aecc43a14d229df4
naghamz12 <naghamzoabi234@gmai.com>
2023-11-08 03:00:30 -0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/FirebaseServises.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+
+class FirebaseServices {
+    private static FirebaseServices instance;
+    private FirebaseFirestore fire;
+    private FirebaseStorage storage;
+    private FirebaseAuth auth;
+
+    public static FirebaseServices getInstance() {
+        if (instance == null)
+            instance = new FirebaseServices();
+        return instance;
+    }
+
+    public FirebaseAuth getAuth() {
+        return auth;
+    }
+
+    public FirebaseFirestore getFire() {
+        return fire;
+    }
+
+    public FirebaseStorage getStorage() {
+        return storage;
+    }
+
+    public FirebaseServices() {
+        auth = FirebaseAuth.getInstance();
+        fire = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance();
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button gallery = findViewById(R.id.gallery);
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 3);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode, data);
+        if(resultCode == RESULT_OK && data !=null){
+            
+        }
+    }
+}
